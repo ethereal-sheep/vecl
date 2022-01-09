2302e77bd8dd6b37a7890d254b15261b8649fdef
GaryChuan <garychuanzh@gmail.com>
2022-01-09 19:43:46 +0800

    Basic setup of poly class

--- include/vecl/poly.hpp
+++ include/vecl/poly.hpp
@@ -1,1 +1,167 @@
 #pragma once
+
+#include <any>
+#include <tuple>
+#include <iostream>
+#include <typeinfo>
+#include <vecl/type_traits.hpp>
+
+namespace vecl
+{
+	template<auto... T>
+	struct value_list {
+		using type = value_list;
+
+		static constexpr auto size = sizeof...(T);
+	};
+
+	template<std::size_t, typename>
+	struct value_list_element;
+
+	// recursive case
+	template<std::size_t I, auto Head, auto... Tail >
+	struct value_list_element<I, value_list<Head, Tail...>>
+		: value_list_element<I - 1, value_list<Tail...>> { };
+
+	// base case
+	template<auto Head, auto... Tail >
+	struct value_list_element<0, value_list<Head, Tail...>> {
+		static constexpr auto value = Head;
+	};
+
+	template <std::size_t I, typename List>
+	constexpr auto value_list_element_v = value_list_element<I, List>::value;
+}
+
+namespace vecl
+{
+	template <auto... Ts>
+	using PolyMembers = value_list<Ts...>;
+
+	struct PolyInspector
+	{
+		template<class Type>
+		operator Type&& () const {
+			return static_cast<Type&&>(*this);
+		}
+
+		template <auto Member, typename... Args>
+		PolyInspector invoke(Args&& ...) const { 
+			return *this; 
+		}
+
+		template <auto Member, typename... Args>
+		PolyInspector invoke(Args&& ...) { 
+			return *this;  
+		}
+	};
+
+	template <typename Ret, typename Cls, typename... Args>
+	auto BuildTableEntry(Ret(Cls::*)(Args...))->Ret(*)(std::any&, Args...);
+
+	template <typename Ret, typename Cls, typename... Args>
+	auto BuildTableEntry(Ret(Cls::*)(Args...) const)->Ret(*)(const std::any&, Args...);
+
+	template <typename Concept, std::size_t... Index>
+	decltype(auto) BuildTable(std::index_sequence<Index...>)
+	{
+		using Inspector = Concept:: template Interface<PolyInspector>;
+		using Members = Concept:: template Members<Inspector>;
+		return std::make_tuple(BuildTableEntry(vecl::value_list_element_v<Index, Members>)...);
+	}
+
+	template <typename Concept>
+	decltype(auto) BuildTable()
+	{
+		using Inspector = Concept:: template Interface<PolyInspector>;
+		using Members = Concept:: template Members<Inspector>;
+		return BuildTable<Concept>(std::make_index_sequence<Members::size>{});
+	}
+
+	template <typename Type, auto Candidate, typename Ret, typename Any, typename... Args>
+	decltype(auto) BuildTableEntry()
+	{
+		return +[](Any& any, Args... args)
+		{
+			return std::invoke(Candidate, std::any_cast<Type>(any), std::forward<Args>(args)...);
+		};
+	}
+
+	template <typename Type, auto Candidate, typename Ret, typename Any, typename... Args>
+	auto BuildTableEntry(Ret(*)(Any&, Args...))
+	{
+		return BuildTableEntry<Type, Candidate, Ret, Any, Args...>();
+	}
+
+	template <typename Concept, typename Type, std::size_t... Index>
+	constexpr decltype(auto) FillTable(std::index_sequence<Index...>)
+	{
+		using Table = decltype(BuildTable<Concept>());
+		using Members = Concept:: template Members<Type>;
+
+		static constexpr Table table;
+		return std::make_tuple(
+			BuildTableEntry<Type, vecl::value_list_element_v<Index, Members>>(
+				std::get<Index>(table))...);
+	}
+
+	template <typename Concept, typename Type>
+	decltype(auto) FillTable()
+	{
+		using Members = Concept:: template Members<Type>;
+		static auto table = FillTable<Concept, Type>(std::make_index_sequence<Members::size>{});
+
+		return &table;
+	}
+
+	template <typename Poly>
+	struct PolyBase
+	{
+		template <auto Member, typename... Args>
+		[[nodiscard]] decltype(auto) invoke(
+			const PolyBase& self, Args&& ... args) const
+		{
+			const auto& poly = static_cast<const Poly&>(self);
+			const auto& vtable = *poly._vtable;
+
+			return std::get<Member>(vtable)(poly._underlying, std::forward<Args>(args)...);
+		}
+
+		template <auto Member, typename... Args>
+		[[nodiscard]] decltype(auto) invoke(
+			PolyBase& self, Args&& ... args)
+		{
+			auto& poly = static_cast<Poly&>(self);
+			const auto& vtable = *poly._vtable;
+
+			return std::get<Member>(vtable)(poly._underlying, std::forward<Args>(args)...);
+		}
+	};
+	
+	template <typename Concept>
+	class Poly : public Concept:: template Interface<PolyBase<Poly<Concept>>>
+	{
+	public:
+		using VTable = decltype(BuildTable<Concept>());
+
+		std::any	  _underlying;
+		const VTable* _vtable;
+
+		template <typename Any>
+		Poly(Any&& any)
+			: _underlying{std::forward<Any>(any)}
+			, _vtable { FillTable<Concept, Any>() }
+		{
+		}
+	};
+
+	template<auto Member, typename Poly, typename... Args>
+	decltype(auto) PolyCall(Poly&& self, Args&& ...args)
+	{
+		return std::forward<Poly>(self). 
+				template invoke<Member>(self, std::forward<Args>(args)...);
+	}
+}
+
+
+
--- test/vecl/poly.cpp
+++ test/vecl/poly.cpp
@@ -1,7 +1,39 @@
 #include <vecl/poly.hpp>
 #include <gtest/gtest.h>
 
+struct Shape
+{
+	template <typename Base>
+	struct Interface : public Base
+	{
+		void draw() const { vecl::PolyCall<0>(*this); }
+		int transform(float f) const { return vecl::PolyCall<1>(*this, f); }
+	};
+
+	template <typename T>
+	using Members = vecl::PolyMembers<&T::draw, &T::transform>;
+};
+
+struct Square
+{
+	void draw() const { std::cout << "Draw Square" << '\n'; }
+	int transform(float) const { std::cout << "Transform Square" << '\n'; return 1; }
+};
+
+struct Circle
+{
+	void draw() const { std::cout << "Draw Circle" << '\n'; }
+	int transform(float) const { std::cout << "Transform Circle" << '\n'; return 1; }
+};
+
+
 TEST(_poly, poly)
 {
-	std::cout << "Poly" << std::endl;
+	std::vector<vecl::Poly<Shape>> shapes;
+
+	shapes.emplace_back(Square{});
+	shapes.emplace_back(Circle{});
+
+	for (const auto& shape : shapes)
+		shape.draw();
 }
